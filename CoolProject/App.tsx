12f6b7702dfc4b911c6f975bc637e2165d22cda3
Mackenzie Williams <62068335+Macca050598@users.noreply.github.com>
2024-01-10 21:32:51 +0000

    CoolProject2

---
+++ CoolProject/App.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import FlatCards from './components/FlatCards'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+      <FlatCards>
+
+      </FlatCards>
+      </ScrollView>
+      
+    </SafeAreaView>
+  )
+}
+
+export default App
